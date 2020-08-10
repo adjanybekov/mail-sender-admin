c8d09437399dfbeed1bb773c2fa46fa619159c24
adjanybekov <adjanybekov@gmail.com>
2020-08-11 01:34:25 +0600

    removed junk code

--- src/pages/MailCompose/MailComposePageImpl.js
+++ src/pages/MailCompose/MailComposePageImpl.js
@@ -15,7 +15,7 @@ export function MailComposePageImpl(props) {
   const [text, setText] = useState("");
 
   const handleSubmit = () => {
-    console.log("submitted", { to, subject, text });
+    console.log("submitted", { receivers, subject, text });
     mailService.createMail({ receivers, subject, text }).then((res) => {
       console.log(res.data);
     });
@@ -32,6 +32,7 @@ export function MailComposePageImpl(props) {
   };
   useEffect(() => {
     console.log(store.emails.slice(), "props storage");
+    setReceivers(store.emails.slice());
   }, []);
 
   return useObserver(() => (
@@ -58,95 +59,6 @@ export function MailComposePageImpl(props) {
         <section class="content">
           <div class="container-fluid">
             <div class="row">
-              {/* <div class="col-md-3">
-                <a href="mailbox.html" class="btn btn-primary btn-block mb-3">
-                  Back to Inbox
-                </a>
-
-                <div class="card">
-                  <div class="card-header">
-                    <h3 class="card-title">Folders</h3>
-
-                    <div class="card-tools">
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-card-widget="collapse"
-                      >
-                        <i class="fas fa-minus"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="card-body p-0">
-                    <ul class="nav nav-pills flex-column">
-                      <li class="nav-item active">
-                        <a href="#" class="nav-link">
-                          <i class="fas fa-inbox"></i> Inbox
-                          <span class="badge bg-primary float-right">12</span>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="far fa-envelope"></i> Sent
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="far fa-file-alt"></i> Drafts
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="fas fa-filter"></i> Junk
-                          <span class="badge bg-warning float-right">65</span>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="far fa-trash-alt"></i> Trash
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                <div class="card">
-                  <div class="card-header">
-                    <h3 class="card-title">Labels</h3>
-
-                    <div class="card-tools">
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-card-widget="collapse"
-                      >
-                        <i class="fas fa-minus"></i>
-                      </button>
-                    </div>
-                  </div>
-
-                  <div class="card-body p-0">
-                    <ul class="nav nav-pills flex-column">
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">
-                          <i class="far fa-circle text-danger"></i> Important
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">
-                          <i class="far fa-circle text-warning"></i> Promotions
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">
-                          <i class="far fa-circle text-primary"></i> Social
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div> */}
-
               <div class="col-md-12">
                 <div class="card card-primary card-outline">
                   <div class="card-header">
