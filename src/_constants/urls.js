5ba9433803d5c518d81802bcbcda20bbaa3799dc
adjanybekov <adjanybekov@gmail.com>
2020-08-12 03:33:00 +0600

    url change

--- src/_constants/urls.js
+++ src/_constants/urls.js
@@ -1,5 +1,6 @@
-export const baseIP = "http://localhost:8090";
-export const serverIP = "http://176.126.167.34";
+export const serverIP = "http://18.191.142.56";
+export const baseIP = serverIP + ":8090"; //"http://localhost:8090";
+
 export const urls = {
   mailUrl: baseIP + "/api/mail",
   emailUrl: baseIP + "/api/email",
